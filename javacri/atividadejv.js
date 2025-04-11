64bbeada2fa81ea336b86816fb0f11ba8d0a8896
daniellima34 <danielgomes29031991@gmail.comm>
2025-04-11 12:05:07 -0300

    exercicios jav

--- javacri/atividadejv.js
+++ javacri/atividadejv.js
@@ -1,9 +1,1 @@
-const nome = "daniel"
-const idade = 34
-const altura = 1.76
-const maiorDeIdade = true
-
-console.log("olá seu nome é " + nome + ", você tem " + idade + " e sua altura é " + altura ) 
-console.log("olá você é maior de idade? " + maiorDeIdade)
-const frase = `olá seu nome é ${nome}`
-console.log(frase)  
+console.log("olá mundo!");
