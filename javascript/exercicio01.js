c5a3d39ce835eb355675826f99b4342a093a39d6
Daniel Gomes <danielgomes29031991@gmail.com>
2025-04-15 23:40:45 -0300

    nova atualização

---
+++ javascript/exercicio01.js
@@ -0,0 +1,18 @@
+const nome= "daniel"
+const idade= 34
+const altura = 1.70
+const maiorDeIdade= true
+console.log ("olá seu nome é " + nome + ",  voce tem " + idade + " anos e sua altura é " + altura)
+
+//olá você é maior de idade ? true 
+
+
+console.log ("você é maior de idade ? " + maiorDeIdade ) 
+
+//template string ou string dinamica 
+
+//const frase= `olá seu nome é ${nome}` 
+//console.log(frase)//
+
+console.log ("1" + 10) //ele vai concatenar 
+console.log (1+10) //ele vai somar 
---
+++ javascript/exercicio08.js
@@ -0,0 +1,10 @@
+//Objeto -object
+const user ={
+ nome:"daniel" ,   //virgula é para separar os valores 
+ idade: 34,
+ cidade:"fortaleza"
+
+}
+
+console.log(user)  //posso chamar o objeto
+console.log(user.idade) // .propriedade user.idade
